9004e52abb452c29049fb89d1c8825b26910121c
Rahul Kavale <kavale.rahul@gmail.com>
2015-06-15 12:01:03 +0530

    Rahul Kavale - added datasets in resources directory, small change in code

--- src/main/scala/sparkExamples/olympicsData/OlympicsAnalysis.scala
+++ src/main/scala/sparkExamples/olympicsData/OlympicsAnalysis.scala
@@ -4,20 +4,24 @@ import org.apache.spark.SparkContext.rddToPairRDDFunctions
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-//data from https://github.com/mortardata/mortar-examples/blob/master/data/OlympicAthletes.csv
-case class Record(name: String, country: String, year: Int, category: String, gold: Int, silver: Int, bronze: Int, total: Int)
+case class Record(name: String,
+                  country: String,
+                  year: Int,
+                  category: String,
+                  gold: Int,
+                  silver: Int,
+                  bronze: Int,
+                  total: Int)
 
 object OlympicsAnalysis {
 
   def main(arg: Array[String]) = {
 
-    val argsValid = arg.size == 3
+    val master = arg(0)
 
-    val master = if (argsValid) arg(0) else "yarn-client"
+    val inputFilePath = arg(1)
 
-    val inputFilePath = if (argsValid) arg(1) else "hdfs:///tmp/olympics-data.csv"
-
-    val outputFilePath = if (argsValid) arg(2) else "hdfs:///tmp/olympics-aggregations"
+    val outputFilePath = arg(2)
 
     val sc = new SparkContext(
       new SparkConf()
@@ -42,6 +46,6 @@ object OlympicsAnalysis {
 
     val parsedRecords = input.map(parse)
 
-    parsedRecords.map(record => (record.country, record.total)).reduceByKey(_ + _).sortBy { case (country, total) => -total}
+    parsedRecords.map(record => (record.country, record.total)).reduceByKey(_ + _).sortBy { case (country, total) => -total }
   }
 }
